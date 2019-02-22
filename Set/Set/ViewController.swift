32bad8145114af9449d7198f5e3fc52b519743f9
Teo <zengwc.teo2016@outlook.com>
2019-02-22 20:23:45 +0800

    the app is now compatible to every ios device

--- Set/Set/ViewController.swift
+++ Set/Set/ViewController.swift
@@ -85,37 +85,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func dealThreeCards(_ sender: UIButton) {
-//        game.dealThreeCards()
-//        for _ in 1...3 {
-//            let randomIndex = cardButtons.count.arc4random
-//            if !usingButtonsIndices.contains( randomIndex ){
-//                cardButtons[randomIndex].backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-//                usingButtonsIndices += [randomIndex]
-//            }
-//        }
-//        for i in 0...2{
-//            let shape = game.newThreeCards[i].shape
-//            var color: UIColor
-//            var alpha: CGFloat
-//            switch game.newThreeCards[i].strip{
-//            case "a":
-//                alpha = 0.15
-//            case "b":
-//                alpha = 0.5
-//            default:
-//                alpha = 1.0
-//            }
-//            switch game.newThreeCards[i].color{
-//            case "Red":
-//                color = UIColor(red: 222/255.0, green: 0/255.0, blue: 0/255.0, alpha: alpha)
-//            case "Blue":
-//                color = UIColor(red: 0/255.0, green: 0/255.0, blue: 222/255.0, alpha: alpha)
-//            default:
-//                color = UIColor(red: 76/255.0, green: 170/255.0, blue: 0/255.0, alpha: alpha)
-//            }
-            //cardButtons[usingButtonsIndices[]].setTitle(shape, for: UIControl.State.normal)
-            //cardButtons[usingButtonsIndices[]].setTitleColor(color, for: UIControl.State.normal)
-        //}
+ 
     }
     
     @IBAction func cheat(_ sender: UIButton) {
